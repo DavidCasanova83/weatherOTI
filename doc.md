3adeb13f1f0d82d3e506761615c5586b0a3f1ef0
david <dvd8301@ubuntu.lan>
2024-05-30 17:47:22 +0200

    add com explicite

--- doc.md
+++ doc.md
@@ -1,17 +1,26 @@
 # Infos utiles
 
-## gpt discussion
+## Gpt discussion
 
 https://chatgpt.com/share/a342abe4-7abe-481c-bb19-cfa0dc16d6d4
 
-## canva automatiser avec des variables
+## Canva automatiser avec des variables
 
 https://www.youtube.com/watch?v=161il_odoc8
 
-Le lien des adresses pour récupérer les informations :
+## Tuto weather Javascript 
+
+https://www.youtube.com/watch?v=qC_DtdhH-J4
+
+
+
+## Le lien des adresses pour récupérer les informations :
 
 Colmars - 04370 => https://www.meteociel.fr/previsions/1597/colmars.htm
 Saint-André-les-Alpes - 04170 => https://www.meteociel.fr/previsions/1689/saint_andre_les_alpes.htm
 La Palud-sur-Verdon - 04120 => https://www.meteociel.fr/previsions/1665/la_palud_sur_verdon.htm
 Annot - 04240 => https://www.meteociel.fr/previsions/1556/annot.htm
 Entrevaux - 04320 => https://www.meteociel.fr/previsions/1610/entrevaux.htm
+
+
+## Faire
--- scrape_weather.py
+++ scrape_weather.py
@@ -11,33 +11,34 @@ def scrape_weather_data(url):
     table = soup.find('table', {'style': 'border-collapse: collapse;'})
     
     # Extraire les données météorologiques
-    data = []
-    for row in table.find_all('tr')[2:]:  # Skipping header rows
-        cols = row.find_all('td')
-        if len(cols) > 10:  # Pour s'assurer que la ligne contient des données météorologiques
-            day = cols[0].text.strip()
-            time = cols[1].text.strip()
-            temp = cols[2].text.strip().split(' ')[0]
-            wind_direction = cols[4].find('img')['title']
-            wind_speed = cols[5].text.strip()
-            wind_gust = cols[6].text.strip()
-            rain = cols[7].text.strip()
-            humidity = cols[8].text.strip()
-            pressure = cols[9].text.strip()
-            weather = cols[10].find('img')['title']
-            
-            data.append({
-                'day': day,
-                'time': time,
-                'temperature': temp,
-                'wind_direction': wind_direction,
-                'wind_speed': wind_speed,
-                'wind_gust': wind_gust,
-                'rain': rain,
-                'humidity': humidity,
-                'pressure': pressure,
-                'weather': weather
-            })
+    data = []  # Créer une liste vide pour stocker les données météorologiques
+    for row in table.find_all('tr')[2:]:  # Parcourir chaque ligne de la table à partir de la troisième ligne (en sautant les lignes d'en-tête)
+      cols = row.find_all('td')  # Trouver toutes les colonnes de la ligne
+      if len(cols) > 10:  # Vérifier si la ligne contient plus de 10 colonnes (pour s'assurer qu'elle contient des données météorologiques)
+        day = cols[0].text.strip()  # Extraire le jour de la colonne 0 et supprimer les espaces inutiles
+        time = cols[1].text.strip()  # Extraire l'heure de la colonne 1 et supprimer les espaces inutiles
+        temp = cols[2].text.strip().split(' ')[0]  # Extraire la température de la colonne 2, supprimer les espaces inutiles et prendre seulement la partie avant l'espace
+        wind_direction = cols[4].find('img')['title']  # Trouver l'élément img dans la colonne 4 et extraire le titre (direction du vent)
+        wind_speed = cols[5].text.strip()  # Extraire la vitesse du vent de la colonne 5 et supprimer les espaces inutiles
+        wind_gust = cols[6].text.strip()  # Extraire les rafales de vent de la colonne 6 et supprimer les espaces inutiles
+        rain = cols[7].text.strip()  # Extraire les précipitations de la colonne 7 et supprimer les espaces inutiles
+        humidity = cols[8].text.strip()  # Extraire l'humidité de la colonne 8 et supprimer les espaces inutiles
+        pressure = cols[9].text.strip()  # Extraire la pression de la colonne 9 et supprimer les espaces inutiles
+        weather = cols[10].find('img')['title']  # Trouver l'élément img dans la colonne 10 et extraire le titre (conditions météorologiques)
+        
+        # Ajouter les données extraites à la liste des données
+        data.append({
+          'day': day,
+          'time': time,
+          'temperature': temp,
+          'wind_direction': wind_direction,
+          'wind_speed': wind_speed,
+          'wind_gust': wind_gust,
+          'rain': rain,
+          'humidity': humidity,
+          'pressure': pressure,
+          'weather': weather
+        })
     
     return data
 
